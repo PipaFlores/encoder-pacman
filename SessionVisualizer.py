9f280cf8fa140c88dffcf24629e7b7b1cbf8314b
Pablo Flores <pablo.flores@helsinki.fi>
2024-12-10 17:39:32 +0200

    Aligned Astar grid to unity frame of reference using 0.5 steps. extended visualization labels

--- SessionVisualizer.py
+++ SessionVisualizer.py
@@ -8,19 +8,24 @@ import Astar
 import logging
 
 class SessionVisualizer:
-    def __init__(self, data, game_id=None, playback_speed=1.0, columns=None, verbose = False, Astar=True, pellets=True, show_grid=False):
+    def __init__(self, data, game_id=None, playback_speed=1.0, columns=None, verbose = False, pathfinding=True, pellets=True, show_grid=False):
         """
         Initialize the visualizer with data.
         
         Args:
             data (DataFrame): Game state data with columns [time, Pacman_X, Pacman_Y, Ghost1_X, Ghost1_Y, ...].
+            game_id (int): Game ID to visualize. If None, the first game ID will be used.
             playback_speed (float): Initial speed multiplier for the playback.
             columns (list): List of columns to use for the animation. If None, the default columns will be used.
+            verbose (bool): Whether to print verbose logging.
+            pathfinding (bool): Whether to calculate A* pathfinding.
+            pellets (bool): Whether to show pellets.
+            show_grid (bool): Whether to show the grid.
         """
         self.full_data = data
         self.game_id = game_id
         self.default_columns = ['game_state_id', 'game_id', 'time_elapsed', 'Pacman_X', 'Pacman_Y', 'Ghost1_X', 'Ghost1_Y',
-                              'Ghost2_X', 'Ghost2_Y', 'Ghost3_X', 'Ghost3_Y', 'Ghost4_X', 'Ghost4_Y']
+                              'Ghost2_X', 'Ghost2_Y', 'Ghost3_X', 'Ghost3_Y', 'Ghost4_X', 'Ghost4_Y', 'score', 'lives']
         self.columns = columns if columns else self.default_columns
 
         # Animation
@@ -33,11 +38,12 @@ class SessionVisualizer:
         self.show_grid = show_grid
         self.pellets = pellets
         self.wall_positions, self.pellet_positions = load_maze_data()
+        self.wall_grid = Astar.generate_squared_walls(self.wall_positions)
         self.pellet_objects = []  # Store pellet objects
         self.eaten_pellets = set()  # Track eaten pellets
 
         # Astar paths
-        self.Astar = Astar
+        self.pathfinding = pathfinding
 
         # Logging
         self.logger = logging.getLogger('SessionVisualizer')
@@ -81,20 +87,23 @@ class SessionVisualizer:
          # Initialize plot elements
          # Add walls and pellets
         for x, y in self.wall_positions:
-            ax_game.add_patch(plt.Rectangle((x, y), 1, -1, 
-                                          color='blue', alpha=0.5)) # FIXME: Check this position transformation
+            ax_game.add_patch(plt.Rectangle((x, y), 1, -1,  # Negative height to assimilate unity's drawing logic.
+                                          color='blue', alpha=0.5)) 
         
         if self.pellets:
             self.pellet_objects = []
             for x, y in self.pellet_positions:
-                pellet = ax_game.plot([x+0.5], [y-0.5], 'o', color='white', markersize=2)[0] # FIXME check this position transformation
+                x = x + 0.5 # Translation to the center of the grid cell.
+                y = y - 0.5
+                pellet = ax_game.plot([x], [y], 'o', color='white', markersize=2)[0]
                 self.pellet_objects.append((pellet, (x, y)))
 
         pacman_dot, = ax_game.plot([], [], 'o', color='yellow', label='Pac-Man')
-        ghost_dots = [ax_game.plot([], [], 'o', label=f'Ghost {i+1}')[0] for i in range(4)]
 
-        path_lines = [ax_game.plot([], [], 'o', color='gray', alpha=0.5, markersize=2)[0] for _ in range(len(ghost_dots))]
-        
+        ghost_colors = ['red', 'pink', 'cyan', 'orange']
+        ghost_dots = [ax_game.plot([], [], 'o', color=ghost_colors[i], label=f'Ghost {i+1}')[0] for i in range(4)]
+
+        path_lines = [ax_game.plot([], [], 'o', color=ghost_colors[i], alpha=0.5, markersize=2)[0] for i in range(len(ghost_dots))]
         # Add game ID text label
         game_id_text = ax_game.text(0.02, 0.98, '', transform=ax_game.transAxes,
                                    verticalalignment='top',
@@ -106,8 +115,18 @@ class SessionVisualizer:
                                    verticalalignment='top',
                                    fontsize=10,
                                    color='white')
+
+        score_text = ax_game.text(0.02, 0.86, '', transform=ax_game.transAxes,
+                                   verticalalignment='top',
+                                   fontsize=10,
+                                   color='white')
         
-        # TODO: Include other indicators such as score, level and lives.
+        lives_text = ax_game.text(0.02, 0.80, '', transform=ax_game.transAxes,
+                                   verticalalignment='top',
+                                   fontsize=10,
+                                   color='white')
+        
+
         
         # Game Selector
         game_ids = sorted(self.full_data['game_id'].unique())
@@ -132,11 +151,14 @@ class SessionVisualizer:
 
             game_id_text.set_text('')
             time_elapsed_text.set_text('')
-            return [pacman_dot, *ghost_dots, game_id_text, time_elapsed_text, *path_lines]
+            score_text.set_text('')
+            lives_text.set_text('')
+
+            return [pacman_dot, *ghost_dots, game_id_text, time_elapsed_text, score_text, lives_text, *path_lines]
         
         def update(frame):
             if not self.is_playing:
-                return [pacman_dot, *ghost_dots, game_id_text, time_elapsed_text, *path_lines,*[p[0] for p in self.pellet_objects]]
+                return [pacman_dot, *ghost_dots, game_id_text, time_elapsed_text, score_text, lives_text, *path_lines,*[p[0] for p in self.pellet_objects]]
             
 
             try:
@@ -147,19 +169,19 @@ class SessionVisualizer:
                 pacman_dot.set_data([pacman_x], [pacman_y])
                 ghost_positions = [(row[f'Ghost{i+1}_X'], row[f'Ghost{i+1}_Y']) for i in range(4)]
 
-                if self.Astar:
+                if self.pathfinding:
                     self.logger.debug(f'Calculating paths for gamestate {row["game_state_id"]} at frame {frame}')
                     self.logger.debug(f'Pacman position: {pacman_x}, {pacman_y}')
                     self.logger.debug(f'Ghost positions: {ghost_positions}')
-                    results = Astar.calculate_ghost_paths_and_distances((pacman_x, pacman_y), ghost_positions, self.wall_positions)
+                    results = Astar.calculate_ghost_paths_and_distances((pacman_x, pacman_y), ghost_positions, self.wall_grid)
                     self.logger.debug(f'Distance to red: {results[0][1]}')
                     paths = [result[0] for result in results]
                     # Draw paths
                     for path_line, path in zip(path_lines, paths):
                         if path:
                             path_x, path_y = zip(*path)
-                            path_x = [x + 0.5 for x in path_x] # Transform to unity reference.
-                            path_y = [y - 0.5 for y in path_y]
+                            path_x = [x for x in path_x] # Transform to unity reference.
+                            path_y = [y for y in path_y]
                             path_line.set_data(path_x, path_y) 
                         else:
                             path_line.set_data([], [])
@@ -167,11 +189,9 @@ class SessionVisualizer:
                 if self.pellets:
                     # Check for pellet collisions
                     for pellet, (x, y) in self.pellet_objects:
-                        pellet_x = x + 0.5
-                        pellet_y = y - 0.5
-                        
-                        distance = ((pellet_x - pacman_x)**2 + (pellet_y - pacman_y)**2)**0.5
-                        if distance < 0.55 and (x, y) not in self.eaten_pellets:
+
+                        distance = ((x - pacman_x)**2 + (y - pacman_y)**2)**0.5
+                        if distance < 0.625 and (x, y) not in self.eaten_pellets:
                             self.logger.debug(f"Eating pellet at {x}, {y}. Distance: {distance:.2f}")
                             pellet.set_visible(False)
                             self.eaten_pellets.add((x, y))
@@ -185,12 +205,15 @@ class SessionVisualizer:
                 time_elapsed = row['time_elapsed']
                 time_elapsed_text.set_text(f'Time Elapsed: {time_elapsed:.2f}')
 
+                score_text.set_text(f'Score: {int(row["score"])}')
+                lives_text.set_text(f'Lives: {int(row["lives"])}')
+
                 if frame == len(self.data) - 2:
                     self.anim.event_source.stop()
                     self.finalized = True
                     self.is_playing = False
                 # Return all artists that need to be redrawn
-                artists = [pacman_dot, *ghost_dots, game_id_text, time_elapsed_text, *[p[0] for p in self.pellet_objects]]
+                artists = [pacman_dot, *ghost_dots, game_id_text, time_elapsed_text, score_text, lives_text, *[p[0] for p in self.pellet_objects]]
                 for path_line in path_lines:
                     if path_line is not None:
                         artists.append(path_line)
@@ -201,7 +224,7 @@ class SessionVisualizer:
                 self.anim.event_source.stop()
                 self.is_playing = False
                 self.finalized = True
-                return [pacman_dot, *ghost_dots, game_id_text, time_elapsed_text, *[p[0] for p in self.pellet_objects]]
+                return [pacman_dot, *ghost_dots, game_id_text, time_elapsed_text, score_text, lives_text, *[p[0] for p in self.pellet_objects]]
         
         def on_game_select(text):
             try:
@@ -258,10 +281,10 @@ if __name__ == "__main__":
     parser.add_argument('--playback-speed', type=float, default=2.0, help="Playback speed multiplier")
     parser.add_argument('--data-path', type=str, default='data/gamestate.csv', help="Path to the game state data")
     parser.add_argument('--no-pellets', action='store_false', dest='pellets', help="Disable pellets")
-    parser.add_argument('--no-astar', action='store_false', dest='astar', help="Disable A* path calculation")
+    parser.add_argument('--no-pathfinding', action='store_false', dest='pathfinding', help="Disable A* path finding calculation")
     parser.add_argument('--verbose', action='store_true', default=False, help="Print verbose logging")
     parser.add_argument('--grid',action= 'store_true', default=False, help="Show grid")
     args = parser.parse_args()
     gamestate_df = pd.read_csv(args.data_path, converters={'user_id': lambda x: int(x)})
-    visualizer = SessionVisualizer(gamestate_df, game_id=args.game_id, playback_speed=args.playback_speed, verbose=args.verbose, pellets=args.pellets, Astar=args.astar, show_grid=args.grid)
+    visualizer = SessionVisualizer(gamestate_df, game_id=args.game_id, playback_speed=args.playback_speed, verbose=args.verbose, pellets=args.pellets, pathfinding=args.pathfinding, show_grid=args.grid)
     visualizer.animate_session()
