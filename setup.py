dae29d93e48eb2810369d1308e4af72c42682895
Pablo Flores <pablo.flores@helsinki.fi>
2025-03-24 16:30:03 +0200

    installable

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="encoder-pacman",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+        "pandas",
+        "ipympl",
+        "seaborn",
+        "stumpy",
+        "lightning",
+    ],
+    python_requires=">=3.11",
+) 
