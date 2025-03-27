f8125c6c890137c725387033bb2089aabcde3300
Pablo Flores <pablo.flores@helsinki.fi>
2025-03-27 16:27:56 +0200

    measure types and clustering skeleton

---
+++ src/analysis/geom_clustering.py
@@ -0,0 +1,38 @@
+import numpy as np
+from typing import List
+from src.analysis.distance_measures import DistanceMeasures
+
+
+class GeomClustering:
+    def __init__(self, distance_measure: str = 'euclidean'):
+        self.distance_measures = DistanceMeasures(distance_measure)
+        self.distance_matrix = np
+
+    def fit(self, trajectories: List[np.ndarray]):
+        self.distance_matrix = self.calculate_distance_matrix(trajectories)
+        self.clusters = self.cluster_trajectories()
+        return self.clusters
+
+    def calculate_distance_matrix(self, trajectories: List[np.ndarray]) -> np.ndarray:
+        """
+        Calculate distance matrix between all trajectories in the list.
+        
+        Args:
+            trajectories: List of arrays (x, y) representing trajectories
+
+        Returns:
+            np.ndarray: Distance matrix between all trajectories
+        """
+        num_trajectories = len(trajectories)
+        distance_matrix = np.zeros((num_trajectories, num_trajectories))
+
+        for i in range(num_trajectories):
+            for j in range(i + 1, num_trajectories):
+                distance_matrix[i, j] = self.distance_measures.calculate_distance(trajectories[i], trajectories[j])
+                distance_matrix[j, i] = distance_matrix[i, j]
+
+        return distance_matrix
+    
+    def cluster_trajectories(self) -> List[np.ndarray]:
+        pass
+
